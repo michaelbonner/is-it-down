bc4d010b28b5a8b9f65dbedb736dee04d17940b3
Michael Bonner <michaelwbonner@gmail.com>
2019-01-04 08:13:30 -0700

    add google notranslate to meta

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -3,6 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="google" content="notranslate">
 
         <title>@yield('title', 'Is It Down Checker')</title>
 
