23521ac6d8599de4f5f5f037216acb6a7c240bf5
Michael Bonner <michaelwbonner@gmail.com>
2020-10-12 16:43:35 -0600

    login

--- routes/web.php
+++ routes/web.php
@@ -13,22 +13,29 @@
 
 use App\Http\Controllers\SitesController;
 use App\Http\Controllers\SitesDownController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [SitesController::class, 'index'])
-    ->name('site.index');
-Route::get('/site/create', [SitesController::class, 'create'])
-    ->name('site.create');
-Route::get('/site/{site}', [SitesController::class, 'show'])
-    ->name('site.show');
-Route::get('/site/{site}/edit', [SitesController::class, 'edit'])
-    ->name('site.edit');
-Route::post('/site/', [SitesController::class, 'store'])
-    ->name('site.store');
-Route::put('/site/{site}', [SitesController::class, 'update'])
-    ->name('site.update');
-Route::delete('/site/{site}', [SitesController::class, 'destroy'])
-    ->name('site.destroy');
+Route::group(['middleware' => 'auth'], function () {
+    Route::get('', [SitesController::class, 'index'])
+        ->name('site.index');
+    Route::get('/site/create', [SitesController::class, 'create'])
+        ->name('site.create');
+    Route::get('/site/{site}', [SitesController::class, 'show'])
+        ->name('site.show');
+    Route::get('/site/{site}/edit', [SitesController::class, 'edit'])
+        ->name('site.edit');
+    Route::post('/site/', [SitesController::class, 'store'])
+        ->name('site.store');
+    Route::put('/site/{site}', [SitesController::class, 'update'])
+        ->name('site.update');
+    Route::delete('/site/{site}', [SitesController::class, 'destroy'])
+        ->name('site.destroy');
 
-Route::get('/sites-down', [SitesDownController::class, 'index'])
-    ->name('sites-down.index');
+    Route::get('/sites-down', [SitesDownController::class, 'index'])
+        ->name('sites-down.index');
+});
+
+Auth::routes([
+    'register' => false
+]);
