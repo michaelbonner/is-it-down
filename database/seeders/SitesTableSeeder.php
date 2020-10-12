8f21c81716c6646423101dc4d498407a608ee3ff
Michael Bonner <michaelwbonner@gmail.com>
2020-10-12 16:16:40 -0600

    add missing files

---
+++ database/seeders/SitesTableSeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\Site;
+
+class SitesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $sites = collect(
+            [
+                'https://www.google.com/',
+                'https://www.lineskis.com/',
+                'https://www.brightonresort.com/',
+                'https://httpstat.us/400',
+                'https://httpstat.us/500',
+                'https://httpstat.us/301',
+                'https://expired.badssl.com/',
+                'https://self-signed.badssl.com/',
+            ]
+        );
+
+        $sites->each(function ($site) {
+            factory(Site::class)->create([
+                'url' => $site,
+            ]);
+        });
+    }
+}
