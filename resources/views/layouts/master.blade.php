cb37b3c4afbff8c05c3ec4d1db58d2be51099df6
Michael Bonner <michaelwbonner@gmail.com>
2020-10-12 19:44:11 -0600

    add refresh token link

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -1,37 +1,45 @@
 <!doctype html>
 <html lang="{{ app()->getLocale() }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="google" content="notranslate">
 
-        <title>@yield('title', 'Is It Down Checker')</title>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="google" content="notranslate">
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.1/css/bulma.min.css">
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma-extensions@6.2.7/bulma-tooltip/dist/css/bulma-tooltip.min.css">
-        <style>
-            a {
-                word-break: break-all;
-            }
-        </style>
-        <script defer src="https://use.fontawesome.com/releases/v5.1.0/js/all.js"></script>
+    <title>@yield('title', 'Is It Down Checker')</title>
 
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @include('layouts.partials.alerts')
-            
-            @yield('content')
-        </div>
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.1/css/bulma.min.css">
+    <link rel="stylesheet"
+        href="https://cdn.jsdelivr.net/npm/bulma-extensions@6.2.7/bulma-tooltip/dist/css/bulma-tooltip.min.css">
+    <style>
+        a {
+            word-break: break-all;
+        }
+    </style>
+    <script defer src="https://use.fontawesome.com/releases/v5.1.0/js/all.js"></script>
 
+</head>
 
-        <script
-            src="https://code.jquery.com/jquery-3.3.1.min.js"
-            integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
-            crossorigin="anonymous"></script>
+<body>
+    <div class="flex-center position-ref full-height">
+        @include('layouts.partials.alerts')
 
-        @yield('js')
-    </body>
+        @yield('content')
+    </div>
+
+    <div class="container has-text-right">
+        <a href="{{route('basecamp-redirect.index')}}">
+            <img alt="Refresh basecamp token" src="/img/basecamp-logo.svg" style="width:120px" /><br />
+            Refresh Token
+        </a>
+    </div>
+
+    <script src="https://code.jquery.com/jquery-3.3.1.min.js"
+        integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+
+    @yield('js')
+</body>
+
 </html>
